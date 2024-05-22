48b3a2b4691cc74cf3aa4b8b0612fcccb69f52be
Henry Backman <henry.backman@gmail.com>
2024-05-22 15:55:40 +0200

    init and through chapter 4

---
+++ extra-addons/tutorial/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'New Real Estate TUTORIAL',
+    'description': 'Tutorial exercise module',
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+    ]
+}
